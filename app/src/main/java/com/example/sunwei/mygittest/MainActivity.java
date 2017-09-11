a1887804bb86af5b0e2bc1162d4400a690c07936
SWwei <736151960@qq.com>
2017-09-11 17:36:58 +0800

    2

--- app/src/main/java/com/example/sunwei/mygittest/MainActivity.java
+++ app/src/main/java/com/example/sunwei/mygittest/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         /**
-         *TEST
+         *
          */
         setContentView(R.layout.activity_main);
     }
